a16b38dd4b7fac80463d58a3e225000139ed3c75
MelinaGoula <melinagoula@gmail.com>
2024-11-30 14:35:17 -0500

    add plotting functions

--- loanclassifier/utils/predict.py
+++ loanclassifier/utils/predict.py
@@ -8,6 +8,21 @@ from sklearn.metrics import (
     precision_score
 )
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+
+mpl.rcParams.update({
+    'font.family': 'serif',  # Use serif fonts
+    'font.size': 14,  # General font size
+    'axes.titlesize': 18,  # Title font size
+    'axes.labelsize': 14,  # Label font size
+    'xtick.labelsize': 12,  # X-axis tick font size
+    'ytick.labelsize': 12,  # Y-axis tick font size
+    'legend.fontsize': 14,  # Legend font size
+    'figure.dpi': 300,  # Higher resolution for reports
+    'savefig.dpi': 300,  # Resolution for saved figures
+})
 
 
 def get_predictions(
@@ -47,3 +62,22 @@ def get_predictions(
         }
 
     return results
+
+def plot_roc_curve(results, Y_true):
+    """Plot ROC curves for all models."""
+    fig, ax = plt.figure(figsize=(10, 8))
+    for model_name, result in results.items():
+        y_pred_proba = result["Predicted Probabilities"]
+        fpr, tpr, _ = roc_curve(Y_true, y_pred_proba)        
+        plt.plot(fpr, tpr, label=f"{model_name}")
+
+    ax.plot([0, 1], [0, 1], color="gray", linestyle="--", label="Random guess")
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel("False Positive Rate")
+    ax.set_ylabel("True Positive Rate")
+    ax.set_title("ROC Curves")
+    ax.set_legend(loc="lower right")
+    plt.show()  
+
+    return fig, ax
